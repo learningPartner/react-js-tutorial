8afa38e86fc32b542de486d070ea853b0afa5e92
learningPartner <learningpartnerdigital@gmail.com>
2026-07-10 15:30:49 +0530

    Punt n Delete API

--- src/components/PostApi.tsx
+++ src/components/PostApi.tsx
@@ -12,7 +12,12 @@ interface VendorModel {
 
 function PostApi() {
   const [vendorList, setVendorList] = useState<VendorModel[]>([]);
-  const [vendorObj,setvendorObj] = useState<VendorModel>({contactNo:'',emailId:'',vendorId:0,vendorName:''})
+  const [vendorObj, setvendorObj] = useState<VendorModel>({
+    contactNo: "",
+    emailId: "",
+    vendorId: 0,
+    vendorName: "",
+  });
 
   useEffect(() => {
     getALlVendors();
@@ -25,29 +30,71 @@ function PostApi() {
     setVendorList(result.data);
   };
 
-  const createNewvendor = async () =>{
+  const createNewvendor = async () => {
     debugger;
-    const result =  await axios.post('https://api.freeprojectapi.com/api/BusBooking/PostBusVendor', vendorObj);
+    const result = await axios.post(
+      "https://api.freeprojectapi.com/api/BusBooking/PostBusVendor",
+      vendorObj,
+    );
     debugger;
-    if(result.status == 201) {
-         alert("Vendor Created Success");
-          getALlVendors();
+    if (result.status == 201) {
+      alert("Vendor Created Success");
+      getALlVendors();
     } else {
-        alert("API Error")
+      alert("API Error");
     }
-   
-  }
+  };
 
-  const updateVendorName = (event:any) => {
-    setvendorObj(oldObj => ({...oldObj,vendorName: event.target.value}))
-  }
-  const updateVendorMobile = (event:any) => {
-    setvendorObj(oldObj => ({...oldObj,contactNo: event.target.value}))
-  }
+  const onUpdateVendor = async () => {
+    const result = await axios.put(
+      "https://api.freeprojectapi.com/api/BusBooking/PutBusVendors?id=" +
+        vendorObj.vendorId,
+      vendorObj,
+    );
 
-  const updateFormValue = (event:any, fieldName: string) => {
-    setvendorObj(oldObj => ({...oldObj, [fieldName]: event.target.value}))
-  }
+    if (result.status == 204) {
+      alert("Vendor Updated Successfully");
+      getALlVendors();
+    } else {
+      alert("API Error");
+    }
+  };
+
+  const onDeleteVendor = async (id: number) => {
+    try {
+      const isConfirm = confirm("Are you Sure want to Delete");
+      if (isConfirm == true) {
+        const result = await axios.delete(
+          "https://api.freeprojectapi.com/api/BusBooking/DeleteBusVendor?id=" +
+            id,
+        );
+        if (result.status == 204) {
+          alert("Vendor Delete Successfully");
+          getALlVendors();
+        } else {
+          alert("API Error");
+        }
+      }
+    } catch (error) {
+      debugger;
+      alert("Api Erros : " + error);
+    }
+  };
+
+  const updateVendorName = (event: any) => {
+    setvendorObj((oldObj) => ({ ...oldObj, vendorName: event.target.value }));
+  };
+  const updateVendorMobile = (event: any) => {
+    setvendorObj((oldObj) => ({ ...oldObj, contactNo: event.target.value }));
+  };
+
+  const updateFormValue = (event: any, fieldName: string) => {
+    setvendorObj((oldObj) => ({ ...oldObj, [fieldName]: event.target.value }));
+  };
+
+  const onEdit = (item: VendorModel) => {
+    setvendorObj(item);
+  };
 
   return (
     <div>
@@ -70,7 +117,7 @@ function PostApi() {
                       <th>Vendor Name</th>
                       <th>Contact No</th>
                       <th>Email Id</th>
-                      <th>Action</th>
+                      <th style={{ width: "130px" }}>Action</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -81,10 +128,16 @@ function PostApi() {
                           <td>{data.contactNo}</td>
                           <td>{data.emailId}</td>
                           <td>
-                            <button className="btn btn-sm btn-warning me-2">
+                            <button
+                              className="btn btn-sm btn-warning me-2"
+                              onClick={() => onEdit(data)}
+                            >
                               Edit
                             </button>
-                            <button className="btn btn-sm btn-danger">
+                            <button
+                              className="btn btn-sm btn-danger"
+                              onClick={() => onDeleteVendor(data.vendorId)}
+                            >
                               Delete
                             </button>
                           </td>
@@ -102,14 +155,16 @@ function PostApi() {
               <div className="card-header bg-warning text-dark">
                 <h5 className="mb-0">Vendor Form</h5>
               </div>
-             vendorName - {vendorObj.vendorName} --  mmobile - {vendorObj.contactNo}
+              {vendorObj.vendorId}: vendorName - {vendorObj.vendorName} --
+              mmobile - {vendorObj.contactNo}
               <div className="card-body">
                 <form>
                   <div className="mb-3">
                     <label className="form-label">Vendor Name</label>
                     <input
                       type="text"
-                      onChange={(eve)=>updateFormValue(eve,'vendorName')}
+                      value={vendorObj.vendorName}
+                      onChange={(eve) => updateFormValue(eve, "vendorName")}
                       className="form-control"
                       placeholder="Enter vendor name"
                     />
@@ -118,7 +173,8 @@ function PostApi() {
                     <label className="form-label">Contact No</label>
                     <input
                       type="text"
-                       onChange={(eve)=>updateFormValue(eve,'contactNo')}
+                      value={vendorObj.contactNo}
+                      onChange={(eve) => updateFormValue(eve, "contactNo")}
                       className="form-control"
                       placeholder="Enter contact number"
                     />
@@ -127,15 +183,33 @@ function PostApi() {
                     <label className="form-label">Email Id</label>
                     <input
                       type="email"
-                       onChange={(eve)=>updateFormValue(eve,'emailId')}
+                      value={vendorObj.emailId}
+                      onChange={(eve) => updateFormValue(eve, "emailId")}
                       className="form-control"
                       placeholder="Enter email"
                     />
                   </div>
                   <div className="d-flex gap-2">
-                    <button type="button" onClick={createNewvendor} className="btn btn-warning">
-                      Save
-                    </button>
+                    {vendorObj.vendorId == 0 && (
+                      <button
+                        type="button"
+                        onClick={createNewvendor}
+                        className="btn btn-warning"
+                      >
+                        Save
+                      </button>
+                    )}
+
+                    {vendorObj.vendorId !== 0 && (
+                      <button
+                        type="button"
+                        onClick={onUpdateVendor}
+                        className="btn btn-success"
+                      >
+                        Update User
+                      </button>
+                    )}
+
                     <button type="reset" className="btn btn-secondary">
                       Clear
                     </button>
